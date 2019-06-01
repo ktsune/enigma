8df987de7ccc05ed58001a5342347fb3490be575
Sarah Tatro <statrox4@gmail.com>
2019-05-31 18:06:48 -0600

    Add key & date to get new offset

--- lib/offsets.rb
+++ lib/offsets.rb
@@ -1,8 +1,4 @@
-require 'enumerator'
-
 class Offset
-  attr_reader :key, :date
-
   def initialize(message, key, date)
     @message = message
     @key = key
@@ -11,25 +7,26 @@ class Offset
 
   def split_key
      b = @key.split("").each_cons(2).map do |pair|
-       pair.map do |string_num|
+      a = pair.map do |string_num|
         # binding.pry
         string_num
       end.join("").to_i
     end
-     b.map do |num| num.to_s.split.map do |str| str.to_i end  end
+     # b.map do |num| num.to_s.split.map do |str| str.to_i end  end
     # binding.pry
   end
 
   def transform_date
     squared = @date.to_i**2
     a = squared.to_s.slice(-4..-1).to_i
-    b = a.to_s.split("").each_cons(1).map do |num| p num end
-      # binding.pry
-    b.map do |num| num.map do |num| num.to_i end end
+    a.to_s.chars.map(&:to_i)
+  end
+
+  def combine_date_and_key
+    split_key.zip(transform_date)
   end
 
   def add_key_and_date
-    # split_key.zip(transform_date)
-    # [split_key][transform_date].transpose.map { |pair| pair.reduce(:+) }
+    combine_date_and_key.map do |pair| p pair.sum end
   end
 end
--- test/offsets_test.rb
+++ test/offsets_test.rb
@@ -11,19 +11,27 @@ class OffsetTest < Minitest::Test
 
   def test_it_splits_key
     expected =
-      [02],[27],[71],[15]
+      [02, 27, 71, 15]
 
     assert_equal expected, @offset.split_key
   end
 
   def test_it_transforms_date
-    expected = [1], [0], [2], [5]
+    expected =
+      [1, 0, 2, 5]
+
     assert_equal expected, @offset.transform_date
   end
 
+  def test_it_combines_date_and_key
+    expected = [02, 1], [27, 0], [71, 2], [15, 5]
+    assert_equal expected, @offset.combine_date_and_key
+  end
+
   def test_it_adds_date_and_offset
+    # skip
     expected =
-    [3],[27],[73],[20]
+    [3,27,73,20]
 
     assert_equal expected, @offset.add_key_and_date
   end
