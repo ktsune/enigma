3f9b7577c14fcc2c4567052f0f3a90698e88a18e
Sarah Tatro <statrox4@gmail.com>
2019-06-01 16:05:35 -0600

    Incorporate helper classes into encrypt method

--- test/offsets_test.rb
+++ test/offsets_test.rb
@@ -2,7 +2,7 @@ require './test/test_helper'
 
 class OffsetTest < Minitest::Test
   def setup
-    @offset = Offset.new("hello world", "02715", "040895")
+    @offset = Offset.new( "02715", "040895" )
   end
 
   def test_it_exists
