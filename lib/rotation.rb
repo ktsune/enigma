5ce4034ea2fa35cf9dbd1f2133c7186b638381ff
Sarah Tatro <statrox4@gmail.com>
2019-06-01 08:34:57 -0600

    Add each with index to message and offset on shift method

--- lib/rotation.rb
+++ lib/rotation.rb
@@ -1,3 +1,48 @@
 class Rotation
 
-end 
+  def initialize(message, offset)
+    @message = message
+    @offset = offset
+  end
+
+  def assign_offsets_to_letters
+    offset_hash = {}
+    letters = 'a'..'d'
+
+    @offset.each do |num|
+      letters.each do |letter|
+        offset_hash[letter] = num
+      end
+    end
+    offset_hash
+  end
+
+  def shift
+  str = ""
+  alpha = ("a".."z").to_a << " "
+    # => for each letter in msg,
+    @message.split("").each_with_index do |msg_letter, index_msg|
+    # => use msg index to find offset index
+      @offset.each_with_index do |offset, offset_index|
+      # => look up alpha index using msg letter
+        if index_msg == offset_index
+          if alpha.include?(msg_letter)
+# => use new index to look up new char in alpha
+            str = alpha.rotate(index_msg + offset)
+          end
+        end
+        binding.pry
+      end
+      binding.pry
+    end
+  end
+end
+
+
+# @message.split("").each do |msg_letter|
+  # alpha.with_index do |letter, index|
+  #   if letter == msg_letter
+  #     assign_offsets_to_letters.each do |letter_key, offset_num|
+  #       @message.split("").rotate(index + offset_num)
+  #       # return letter of associated index
+  #         letter
--- test/rotation_test.rb
+++ test/rotation_test.rb
@@ -2,10 +2,32 @@ require './test/test_helper'
 
 class RotationTest < Minitest::Test
   def setup
-    @rotation = Rotation.new("hello world", "02715", "040895")
+    # skip
+    @rotation = Rotation.new("hello world", [3,27,73,20])
   end
 
   def test_it_exists
     assert_instance_of Rotation, @rotation
-  end 
+  end
+
+  # def test_it_assigns_offsets_to_letters
+  #   expected =
+  #     {
+  #       "a" => 3,
+  #       "b" => 27,
+  #       "c" => 73,
+  #       "d" => 20
+  #     }
+  #
+  #   assert_equal expected, @rotation.assign_offsets_to_letters
+  # end
+
+  def map_offset_keys_to_msg
+
+  end
+
+  def test_it_shifts_chars
+    # skip
+    assert_equal "keder ohulw", @rotation.shift
+  end
 end
