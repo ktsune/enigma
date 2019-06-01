15f0e560994b66426ebb944b1e4812bea010bd79
Sarah Tatro <statrox4@gmail.com>
2019-06-01 17:16:31 -0600

    Break encrypt and decrypt out into separate methods

--- lib/rotation.rb
+++ lib/rotation.rb
@@ -5,22 +5,65 @@ class Rotation
     @offset = offset
   end
 
+  # def find_offset(msg_index)
+  #   # => always return remainder of msg_index / offset length as offset index
+  #   offset_index = msg_index % @offset.length
+  #   @offset[offset_index]
+  # end
+  #
+  # def lookup_char(offset_value, msg_letter)
+  #   # => output: new char in alpha
+  #   alpha = ("a".."z").to_a << " "
+  #   found_index = 0
+  #   alpha.each.with_index do |letter, letter_index|
+  #     if letter == msg_letter
+  #       found_index = letter_index
+  #     end
+  #   end
+  #   rotated_alpha = alpha.rotate(offset_value)
+  #   rotated_alpha[found_index]
+  # end
+  #
+  # def shift
+  #   str = ""
+  #   @message.split("").each_with_index do |msg_letter, msg_index|
+  #     offset = find_offset(msg_index)
+  #     new_char = lookup_char(offset, msg_letter)
+  #     str += new_char
+  #   end
+  #   str
+  # end
+
   def find_offset(msg_index)
     # => always return remainder of msg_index / offset length as offset index
     offset_index = msg_index % @offset.length
+    # binding.pry
     @offset[offset_index]
   end
 
   def lookup_char(offset_value, msg_letter)
     # => output: new char in alpha
     alpha = ("a".."z").to_a << " "
+    # binding.pry
     found_index = 0
     alpha.each.with_index do |letter, letter_index|
       if letter == msg_letter
         found_index = letter_index
       end
     end
-    rotated_alpha = alpha.rotate(offset_value)
+  end
+
+  def rotated_alpha_encrypt
+    alpha = ("a".."z").to_a << " "
+
+    alpha.rotate(offset_value)
+    rotated_alpha[found_index]
+  end
+
+  def rotated_alpha_decrypt
+    alpha = ("a".."z").to_a << " "
+
+    alpha.rotate(offset_value * -1)
     rotated_alpha[found_index]
   end
 
--- test/rotation_test.rb
+++ test/rotation_test.rb
@@ -2,7 +2,7 @@ require './test/test_helper'
 
 class RotationTest < Minitest::Test
   def setup
-    @rotation = Rotation.new("hello world", [3,27,73,20])
+    @rotation = Rotation.new("keder ohulw", [3,27,73,20])
   end
 
   def test_it_exists
@@ -10,14 +10,33 @@ class RotationTest < Minitest::Test
   end
 
   def test_find_offset
+    skip
     assert_equal 3, @rotation.find_offset(0)
   end
 
   def test_it_finds_new_char_in_alpha
+    skip
     assert_equal "k", @rotation.lookup_char(3, "h")
   end
 
   def test_it_shifts_chars
+
     assert_equal "keder ohulw", @rotation.shift
   end
+
+  # def test_find_offset
+  #   # skip
+  #   assert_equal (-3), @rotation.find_offset(0)
+  # end
+
+  # def test_it_finds_new_char_in_alpha
+  #   # skip
+  #   assert_equal "h", @rotation.lookup_char(-3, "k")
+  # end
+
+#   def test_it_unshifts_chars
+#     # skip
+#
+#     assert_equal "hello world", @rotation.rotated_alpha_decrypt
+#   end
 end
