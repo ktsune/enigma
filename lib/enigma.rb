8f2a575d82c323205f209f9262f9df8e4301000a
Sarah Tatro <statrox4@gmail.com>
2019-06-01 21:46:43 -0600

    Add optional date param

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,12 @@
 class Enigma
 
-  def encrypt(message, key, date=nil)
+  def encrypt(message, key=nil, date=nil)
     encryption_hash = {}
     offset = Offset.new(key, date)
     new_offset = offset.add_key_and_date
     rotation = Rotation.new(true, message, new_offset)
     encryption_hash[:encryption] = rotation.shift
-    encryption_hash[:key] = key
+    encryption_hash[:key] = offset.key
     encryption_hash[:date] = offset.date
     encryption_hash
   end
@@ -17,7 +17,7 @@ class Enigma
     new_offset = offset.add_key_and_date
     rotation = Rotation.new(false, message, new_offset)
     decryption_hash[:decryption] = rotation.shift
-    decryption_hash[:key] = key
+    decryption_hash[:key] = offset.key
     decryption_hash[:date] = offset.date
     decryption_hash
   end
