99886e320969fc1a36e7c12eef204b757184d067
Sarah Tatro <statrox4@gmail.com>
2019-06-01 20:31:24 -0600

    Test passes it decrypts with todays date

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -37,16 +37,22 @@ class EnigmaTest < Minitest::Test
         key: "02715",
         date: date
       }
-
     assert_equal expected, encrypted
   end
 
-  # def test_it_decrypts_message_with_key_and_todays_date
-  #   skip
-  #   enigma.decrypt(encrypted[:encryption], "02715")
-  #   #=> # decryption hash here
-  # end
-  #
+  def test_it_decrypts_message_with_key_and_todays_date
+    encrypted = @enigma.encrypt("hello world", "02715")
+    date = Date.today.strftime("%d%m%y")
+
+    expected =
+      {
+        decryption: "hello world",
+        key: "02715",
+        date: date
+      }
+    assert_equal expected, @enigma.decrypt(encrypted[:encryption], "02715")
+  end
+
   # def test_it_encrypts_message_using_random_key_and_todays_date
   #   skip
   #   enigma.encrypt("hello world")
