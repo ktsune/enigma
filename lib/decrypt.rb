a5799bbb4268a5c090f743446880e018a3e22b39
Sarah Tatro <statrox4@gmail.com>
2019-06-04 15:23:59 -0600

    Fix Encrypt.rb extra meta data issue

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -11,19 +11,20 @@ message_to_decrypt = handle.read
 
 handle.close
 
+key = ARGV[2]
+date = ARG[3]
+
 enigma = Enigma.new
 decrypt = enigma.decrypt(message_to_decrypt, key=nil, date=nil)
 
 file_to_decrypt = File.open(ARGV[1], "w")
-key = File.open(ARGV[2], "w")
-date = File.open(ARGV[3], "w")
 
-file_to_decrypt.write(decrypt)
-key.write(decrypt[:key])
-date.write(decrypt[:date])
+file_to_decrypt.write(decrypt[:decryption])
+# key.write(decrypt[:key])
+# date.write(decrypt[:date])
 
 puts "Created '#{ARGV[1]}' with the key #{decrypt[:key]} and date #{decrypt[:date]}"
 
 file_to_decrypt.close
-key.close
-date.close
+# key.close
+# date.close
